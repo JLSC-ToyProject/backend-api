fffea9e1e306b10c5febc769ac9cf8393e26f90f
seokheejang <shjang@deleo.co.kr>
2022-02-15 01:03:36 +0900

    feat : Add getKakaoAccessToken library

--- api/login/loginKakao.js
+++ api/login/loginKakao.js
@@ -5,24 +5,35 @@
  */
 
 const request = require('request');
+const fetch = require('node-fetch');
+const qs = require('qs');
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
+class Kakao {
+    constructor(code) {
+        this.url = process.env.KAKAO_TOKEN_URI;
+        this.clientId = process.env.KAKAO_CLIENT_ID;
+        this.clientSecret = process.env.KAKAO_SECRET_KEY;
+        this.redirectUri = process.env.KAKAO_REDIRECT_URI;
+        this.code = code;
+    }
+}
 
 module.exports.Init = () => {
     try {
         const kakao = {
             clientId: process.env.KAKAO_CLIENT_ID,
-            clientSecret: process.env.KAKAO_SECRET,
-            redirectUri: process.env.KAKAO_REDIRECT_URL
+            clientSecret: process.env.KAKAO_SECRET_KEY,
+            redirectUri: process.env.KAKAO_REDIRECT_URI
         }
-        const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientId}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile,account_email`;
+        const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientId}&redirect_uri=${kakao.redirectUri}&response_type=code`;
         request({
             url: kakaoAuthURL,
             method: 'GET'
         }, function (error, response, body) {
-            console.log('Status', response.statusCode);
-            console.log('Headers', JSON.stringify(response.headers));
+            //console.log('Status', response.statusCode);
+            //console.log('Headers', JSON.stringify(response.headers));
             console.log('Reponse received', body);
         });
     } catch(error) {
@@ -30,10 +41,32 @@ module.exports.Init = () => {
     }
 }
 
-module.exports.kakaoLogin = () => {
+module.exports.getKakaoAccessToken = async (code) => {
     try {
-
+        const kakao = new Kakao(code);
+        const token = await getAccessToken(kakao);
+        return token;
     } catch(error) {
         console.log("loginKakao::kakaoLogin::error", error);
     }
 }
+
+const getAccessToken = async (options) => {
+    try {
+        return await fetch(options.url, {
+            method: 'POST',
+            headers: {
+                'content-type':'application/x-www-form-urlencoded;charset=utf-8'
+            },
+            body: qs.stringify({
+                grant_type: 'authorization_code', // 특정 스트링
+                client_id: options.clientId,
+                client_secret: options.clientSecret,
+                redirectUri: options.redirectUri,
+                code: options.code,
+            }),
+        }).then(res => res.json());
+    } catch(error) {
+        console.log("loginKakao::getAccessToken::error", error);
+    }
+};
--- exams/examKakaoLogin.js
+++ exams/examKakaoLogin.js
@@ -5,11 +5,25 @@
  */
 
 const Init = require('../api/login/loginKakao').Init;
-
+const getKakaoAccessToken = require('../api/login/loginKakao').getKakaoAccessToken;
 
 const RunProc = async () => {
     try {
-        const result = Init();
+        if(process.argv.length < 3) {
+            throw new Error("Invalid Parameters!");
+        }
+        const code = process.argv[2];
+        const result = await getKakaoAccessToken(code);
+        
+        /** 
+         * @notice 결과 값 예상 모습
+         * access_token: 'tgE2lJnxp3qjFp-03L8OLQ6ilBjmI0FlHm45CQorDKgAAAF--PZzYQ',
+         * token_type: 'bearer',
+         * refresh_token: 'Gu4pRpGCdxKkXeNNfXbSIUSMjcvB3Z4O-rTPvworDKgAAAF--PZzYA',
+         * expires_in: 21599,
+         * scope: 'profile_nickname',
+         * refresh_token_expires_in: 5183999
+         */
         console.log("RunProc::result", result);
     } catch(error) {
         console.log("RunProc::error", error);
